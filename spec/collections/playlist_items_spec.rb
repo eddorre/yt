abd6bcfc63f9cdad4bf9e1c27319f01af6ee2b7c
Carlos Rodriguez <carlos@eddorre.com>
2025-06-10 19:15:04 -0700

    Collections specs now test for etag support

--- spec/collections/playlist_items_spec.rb
+++ spec/collections/playlist_items_spec.rb
@@ -41,4 +41,20 @@ describe Yt::Collections::PlaylistItems do
 
     it { expect(collection.delete_all).to eq [true] }
   end
+
+  describe '#etag' do
+    let(:etag) { 'etag123' }
+    let(:behave) { receive(:fetch_page).and_call_original }
+
+    before do
+      expect_any_instance_of(Yt::Request).to receive(:run).once do
+        double(body: {'etag'=> etag, 'items'=> [], 'pageInfo'=> {'totalResults'=>0}})
+      end
+    end
+
+    it 'returns the etag from the list response' do
+      collection.count
+      expect(collection.etag).to eq etag
+    end
+  end
 end
